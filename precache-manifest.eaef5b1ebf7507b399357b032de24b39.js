084ef7c14e279eb287e8551dac11b7647060aff0
Dinh Tran <tranquocdinhcse98@outlook.com>
2020-08-18 17:33:51 +0700

    Updates

---
+++ precache-manifest.eaef5b1ebf7507b399357b032de24b39.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9583ce1b94ce87bfa74bc69697148c0",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "457c1c2fe8598ecf96fd",
+    "url": "/my-portfolio/static/css/main.7cf18e30.chunk.css"
+  },
+  {
+    "revision": "30560d93d97912767236",
+    "url": "/my-portfolio/static/js/2.e564a75a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.e564a75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457c1c2fe8598ecf96fd",
+    "url": "/my-portfolio/static/js/main.ddfea51a.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "c71f55cf72cb813b4e3451d06abb05f2",
+    "url": "/my-portfolio/static/media/avatar.c71f55cf.jpg"
+  },
+  {
+    "revision": "5bf7e95784b17397b51a6db8df7002fc",
+    "url": "/my-portfolio/static/media/avatar2.5bf7e957.jpg"
+  },
+  {
+    "revision": "80df5d7c49fa9c3413cb03e5ee117381",
+    "url": "/my-portfolio/static/media/background1.80df5d7c.jpg"
+  },
+  {
+    "revision": "841a5cdbf982a7bc4bd4f484fcd8da72",
+    "url": "/my-portfolio/static/media/project_1 (2).841a5cdb.png"
+  },
+  {
+    "revision": "9a60534e6d648f2cba891dc41eb80817",
+    "url": "/my-portfolio/static/media/project_2.9a60534e.png"
+  },
+  {
+    "revision": "e3ddaf5dc77133ecaf4f1d4221d54d0c",
+    "url": "/my-portfolio/static/media/project_3 (3).e3ddaf5d.png"
+  }
+]);
